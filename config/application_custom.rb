9985712d1ccd4f86470b30eb2915752288deb11b
Diego Calvo <diego.calvo@enreda.coop>
2021-10-28 11:56:46 +0200

    target _blank

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -2,5 +2,8 @@ module Consul
   class Application < Rails::Application
     config.i18n.default_locale = 'es-PE'
     config.i18n.available_locales = ['es-PE']
+    config.after_initialize do
+      ActionView::Base.sanitized_allowed_attributes.add 'target'
+    end
   end
 end
